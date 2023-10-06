48dc7506d6f11e3345f1d92d25e00ab882898939
Hoang Nguyen <nmhoang2909@gmail.com>
2023-10-06 15:52:39 +0700

    add arrayandslice func

--- arrays_and_slices/sum.go
+++ arrays_and_slices/sum.go
@@ -8,3 +8,18 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sum []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sum = append(sum, 0)
+		} else {
+			tail := numbers[1:]
+			sum = append(sum, Sum(tail))
+		}
+	}
+
+	return sum
+}
--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -1,6 +1,7 @@
 package arraysandslices_test
 
 import (
+	"reflect"
 	"testing"
 
 	. "example.com/go-test/arrays_and_slices"
@@ -16,5 +17,28 @@ func TestSum(t *testing.T) {
 		if got != want {
 			t.Errorf("got %d but want %d, given %v", got, want, numbers)
 		}
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSum := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d but want %d", got, want)
+		}
+	}
+
+	t.Run("make the sum of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSum(t, got, want)
+	})
+
+	t.Run("safely sum empty slice", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 9})
+		want := []int{0, 9}
+
+		checkSum(t, got, want)
 	})
 }
